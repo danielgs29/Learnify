051cb11277cdf40c04761df002721f340cd88d7d
danielgs29 <danielgs2929@gmail.com>
2023-01-28 20:40:58 +0100

    primera parte

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = sticky;
+ 
+ function sticky(){
+    const header=document.querySelector("header");
+    console.log(header);
+    
+    window.addEventListener("scroll", function (){
+        header.classList.toggle("sticky", window.scrollY > 0);
+        
+    });
+}
